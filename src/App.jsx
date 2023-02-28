27f9711862ac09ec49885b06c1395fad2c1d712c
Rai-vaz <ray.vaz04@gmail.com>
2023-02-28 06:35:49 -0300

    Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import ImgNewYear from '../src/imagem/fogos-ano-novo.jpg'
-import { Outlet } from 'react-router-dom'
+import Form from './components/Form'
+import CounterRun from './components/CounterRun'
+import {Routes, Route} from 'react-router-dom'
 import './css/App.css'
 
 function App() {
@@ -7,18 +9,11 @@ function App() {
   return (
     <div className="App" style={{backgroundImage: `url(${ImgNewYear})`}}>
       <div className='container'>
-        {/**<Title texto="Contagem regressiva para 2024"/>
-          <div className='countdown-container'>
-          <Counter title="Dias" number={day}/>
-          <Counter title="Horas" number={hour}/>
-          <Counter title="Minutos" number={minute}/>
-          <Counter title="Segundos" number={second}/> 
-         
-        </div>*/}
-        <Outlet/>
-        
+        <Routes>
+          <Route path='/' element={<Form/>}/>
+          <Route path='/counter' element={<CounterRun/>}/>
+        </Routes>
       </div>
-     
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Form from './components/Form'
-import CounterRun from './components/CounterRun'
 import './css/index.css'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 
-const routers = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>,
-    children: [
-      {
-        path: '/',
-        element: <Form/>
-      },
-      {
-        path: '/counter',
-        element: <CounterRun/>
-      }
-    ]
-  }
-])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={routers}/>   
+    <BrowserRouter>
+      <App/> 
+    </BrowserRouter>
   </React.StrictMode>,
 )
